a30ab73154be6942406c8b2eafad78f390fa3122
Guo, Dian <dianguo@gatech.edu>
2019-12-24 14:19:46 -0500

    dummy controller test

--- src/main/java/com/dian/controller/CustomerController.java
+++ src/main/java/com/dian/controller/CustomerController.java
@@ -1,13 +1,16 @@
 package com.dian.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dian.model.Customer;
 import com.dian.service.CustomerService;
 
-@RestController("/api")
+@RestController//("/api")
+@RequestMapping("/api")
 public class CustomerController {
 	
 	private CustomerService customerService;
@@ -23,6 +26,11 @@ public class CustomerController {
 		cus.setFirstName("only firstname");
 		this.customerService.addCustomer(cus);
 		return cus;
+	}
+	
+	@GetMapping("/all") 
+	public String test() {
+		return "something";
 	}
 	
 	
